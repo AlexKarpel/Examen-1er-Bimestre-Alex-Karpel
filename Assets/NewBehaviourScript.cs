0d64a60caddd33f483535ab33455426ef989c871
AlexKarpel <101826650+AlexKarpel@users.noreply.github.com>
2022-04-12 14:12:05 -0300

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public string productos = "";
+    public int precios = 0;
+    public int unidades = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float descuento = precios * unidades * 0.2f;
+        int precio = precios * unidades;
+        float preciocondescuento = precio - descuento;
+
+        if (unidades < 3)
+        {
+            Debug.Log("Para aplicar el descuento, tienen que haber mas de 3 unidades");
+        } else if(unidades > 3)
+        {
+            Debug.Log("El precio total es de"+precio+"pesos, el descuento es de"+descuento+"pesos y  con el descuento te queda en"+preciocondescuento+"pesos");
+        }
+
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
